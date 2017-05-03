3fe4d7459a67a966460cc31ab347aaefc08562e7
AndrewChen <chenpoanandrew@gmail.com>
2017-05-03 11:11:41 +0800

    add index page, close #12

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ let tasks = require('../tasks.js');
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
-router.get('/:page', function(req, res, next) {
+router.get('/', (req, res, next) => {
+  res.render('_index', {tasks});
+});
+
+router.get('/:page', (req, res, next) => {
   let task = tasks.filter( task => task.page == req.params.page)[0];
   res.render(task.view, task.data);
 });
